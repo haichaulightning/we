54c7b5f7a353d99b76853d17a0f20ed4698d6adf
haichaulightning <45975564+haichaulightning@users.noreply.github.com>
2018-12-22 15:11:11 +0000

    Update cfg.php

--- cfg.php
+++ cfg.php
@@ -1,8 +1,9 @@
 <?php
-//ikuti format config di bawah "ticket=tiket_kamu&meid=meid_kamu",
-//jangan ada sepasi
-$config = [
-"ticket=sHp2r312otmCpnGns3mCqJhovNiBh3zMsKqfscC4gpayeoJpf3m7zY65eaezdYllkKC40IB3osyvqnmvwLiLyq2jra6KZM3XjZSKo7-Ik2uRo6zWlGWLoQ &meid=7cbf4a1566fb3d3f",
-  ];
 
-?>
+$ticket = "sHp2r312otmCpnGns3mCqJhovNiBh3zMsKqfscC4gpayeoJpf3m7zY65eaezdYllkKC40IB3osyvqnmvwLiLyq2jra6KZM3XjZSKo7-Ik2uRo6zWlGWLoQ";
+
+$meid = "7cbf4a1566fb3d3f";
+
+$os="android";
+
+$sign="825d613db2303fab627d1ca4cad09cf59671c8149d7cc1f09d4128129bfac394";
