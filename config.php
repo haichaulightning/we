b726f49795037db709e5c00357be13f93ace12d2
haichaulightning <45975564+haichaulightning@users.noreply.github.com>
2018-12-23 03:12:21 +0700

    Update config.php

--- config.php
+++ config.php
@@ -1,9 +1,6 @@
 <?php
 
 $ticket = "sHp2r312otmCpnGns3mCqJhovNiBh3zMsKqfscC4gpayeoJpf3m7zY65eaezdYllkKC40IB3osyvqnmvwLiLyq2jra6KZM3XjZSKo7-Ik2uRo6zWlGWLoQ";
-
-$meid = "7cbf4a1566fb3d3f";
-
+$meid = "7cbf4a1566fb3d3";
 $os="android";
-
 $sign="825d613db2303fab627d1ca4cad09cf59671c8149d7cc1f09d4128129bfac394";
