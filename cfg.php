b7efa19c5b50aa65cc477913335154190817e1fd
haichaulightning <45975564+haichaulightning@users.noreply.github.com>
2018-12-22 15:01:53 +0000

    Update cfg.php

--- cfg.php
+++ cfg.php
@@ -2,7 +2,7 @@
 //ikuti format config di bawah "ticket=tiket_kamu&meid=meid_kamu",
 //jangan ada sepasi
 $config = [
-"ticket=tiket_kamu&meid=meid_kamu",
+"ticket=sHp2r312otmCpnGns3mCqJhovNiBh3zMsKqfscC4gpayeoJpf3m7zY65eaezdYllkKC40IB3osyvqnmvwLiLyq2jra6KZM3XjZSKo7-Ik2uRo6zWlGWLoQ &meid=7cbf4a1566fb3d3f",
   ];
 
 ?>
